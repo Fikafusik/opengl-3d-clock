06bbfadc1c7fb13aab501201adf9ec9586957b1d
Minullin Michael <fikafusik@gmail.com>
2020-03-21 23:43:03 +0300

    Initial commit

---
+++ src/main/java/com/fikafusik/App.java
@@ -0,0 +1,11 @@
+package com.fikafusik;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+    public static void main( String[] args ) {
+        new GUI().run();
+    }
+}
---
+++ src/main/java/com/fikafusik/GUI.java
@@ -0,0 +1,14 @@
+
+package com.fikafusik;
+
+import javax.swing.*;
+
+public class GUI extends JFrame {
+    public GUI() {
+        setSize(800, 600);
+    }
+
+    public void run() {
+        setVisible(true);
+    }
+}
